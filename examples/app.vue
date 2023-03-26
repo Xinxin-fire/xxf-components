ac88b8442122996c1acaa816db40e9446f0e3be9
鑫鑫之火可以撩原 <13166835035@163.com>
2023-03-26 21:23:27 +0800

    版本更新

--- examples/app.vue
+++ examples/app.vue
@@ -1,59 +1,60 @@
 <template>
-  <div id="app" >
+  <div id="app">
     <!-- <XxfFormTable :searchConfig="searchConfig" :tableHead="tableHead" /> -->
     <XxfForm class="xxf-form" :formConfig="formConfig" :form="form" />
   </div>
 </template>
 
 <script>
-
-  export default {
-    name: 'app',
-    data() {
-      return {
-        input: 1,
-        // formConfig: [
-        //   {type: 'xxfInput', prop: 'name', value: '' },
-        //   {type: 'xxfSelect', prop: 'status', value: '' },
-        // ],
-        form: {
-          contract: 1,
-          contractName: '1',
+export default {
+  name: "app",
+  data() {
+    return {
+      input: 1,
+      // formConfig: [
+      //   {type: 'xxfInput', prop: 'name', value: '' },
+      //   {type: 'xxfSelect', prop: 'status', value: '' },
+      // ],
+      form: {
+        contract: 1,
+        contractName: "1",
+      },
+      formConfig: {
+        contract: {
+          type: "xxfSelect",
+          label: "合同类型",
+          options: [
+            { label: "正式合同", value: 1 },
+            { label: "临时合同", value: 2 },
+          ],
         },
-        formConfig: {
-          contract: { 
-            type: 'xxfSelect', 
-            label:'合同类型',  
-            options: [{label :'正式合同', value: 1}, {label :'临时合同', value: 2}], 
-          },
-          contractName: { 
-            type: 'xxfInput', 
-            label:'合同名称', 
-            linkValue: ['contract'], 
-            linkFunction: (form, newValue, oldValue) => {
-              if (newValue === 1) {
-                console.log(this)
-                form.contractName = '正式合同'
-              } else {
-                form.contractName = '临时合同'
-              }
-            }, 
+        contractName: {
+          type: "xxfInput",
+          label: "合同名称",
+          linkValue: ["contract"],
+          linkFunction: (form, newValue, oldValue) => {
+            if (newValue === 1) {
+              console.log(this);
+              form.contractName = "正式合同";
+            } else {
+              form.contractName = "临时合同";
+            }
           },
         },
-        searchConfig: [
-          {type: 'xxfInput', prop: 'name', value: '' },
-          {type: 'xxfSelect', prop: 'status', value: '' },
-        ],
-        tableHead: [
-          {prop: 'name', label: '姓名'},
-          {prop: 'status', label: '状态'},
-          {prop: 'address', label: '地址'}
-        ],
-      }
-    },
-    mounted() {
-    }
-  };
+      },
+      searchConfig: [
+        { type: "xxfInput", prop: "name", value: "" },
+        { type: "xxfSelect", prop: "status", value: "" },
+      ],
+      tableHead: [
+        { prop: "name", label: "姓名" },
+        { prop: "status", label: "状态" },
+        { prop: "address", label: "地址" },
+      ],
+    };
+  },
+  mounted() {},
+};
 </script>
 
 <style>
